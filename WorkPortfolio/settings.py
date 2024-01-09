6a8406963fd00049948e77604b8330a1625a3315
Jericho Cristobal <132733742+EXELCHOx@users.noreply.github.com>
2024-01-09 18:09:48 +0800

    Add files via upload

--- WorkPortfolio/settings.py
+++ WorkPortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4xnbc$1n1i99a-v+v@6o$ck&(@@@^2z!-t(uprupcfyzgxxnxh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-vercel-domain.vercel.app']
+ALLOWED_HOSTS = ['your-vercel-domain.vercel.app', '127.0.0.1']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
